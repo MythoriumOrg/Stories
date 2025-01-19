a14622141d639667049f08262f586b32cadba039
Swan <stbnbasson@gmail.com>
2025-01-19 12:07:32 +0100

    cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import './App.css'
+import SerieCard from "./Components/SerieCard/SerieCard.tsx";
 
 function App() {
 
     return (
-        <>
-        </>
+        <div className="body">
+            <div className="series-cards">
+                <SerieCard></SerieCard>
+                <SerieCard></SerieCard>
+                <SerieCard></SerieCard>
+                <SerieCard></SerieCard>
+                <SerieCard></SerieCard>
+            </div>
+        </div>
     )
 }
 
---
+++ src/Components/SerieCard/SerieCard.tsx
@@ -0,0 +1,11 @@
+import "./SerieCard.css"
+
+function SerieCard() {
+    return (
+        <div className="card-body">
+            <img src="../../../public/serieLogo/lePassageOublie.png"/>
+        </div>
+    )
+}
+
+export default SerieCard
