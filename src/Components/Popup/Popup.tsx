840e556334ea19d36f0c755d2b1bd86bbb586392
Swan <stbnbasson@gmail.com>
2025-01-19 18:36:12 +0100

    popup sans volumeCards

--- src/Components/Popup/Popup.tsx
+++ src/Components/Popup/Popup.tsx
@@ -9,14 +9,15 @@ interface PopupProps {
 }
 
 const Popup: React.FC<PopupProps> = ({ serieData, onClose, isClosing }) => {
+
     return (
         <div className="popup-overlay" onClick={onClose}>
             <div
                 className={`popup-content ${isClosing ? 'zoom-out' : 'zoom-in'}`}
                 onClick={e => e.stopPropagation()}
+                style={{ background: `linear-gradient(to bottom, rgba(47, 47, 47, 0) 20%, rgba(47, 47, 47, 1) 65%), url("../../../public/serieLogo/${serieData.images}/fond.png")` }}
             >
-                {serieData.name}
-                <button onClick={onClose}>Close</button>
+                <img src={`../../../public/serieLogo/${serieData.images}/logoSerie.png`} alt="logo serie" className="popup-logo"/>
             </div>
         </div>
     );
